5bfd37cccb01cd7a96bf06b9a10aa72b2335e98a
rajneshitio <rajneshs@itio.in>
2024-12-02 18:15:30 +0530

    chat update

--- chat/header.php
+++ chat/header.php
@@ -1,6 +1,5 @@
 <?php
-
-if(!isset($chat_id)) session_start();
+if(session_id() === "") session_start();
 ?>
 <!DOCTYPE html>
 <html>
--- chat/index.php
+++ chat/index.php
@@ -1,10 +1,9 @@
 <?php 
 include_once "header.php";
 include_once "../application/config/db.php";
-//$_SESSION['unique_id']='11';
+
 if(isset($_SESSION['unique_id'])&&$_SESSION['unique_id'])
 {
-//	header("Location:php/users.php");
 	header("Location:chat.php");
 	exit;
 }
