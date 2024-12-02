80d04c281cb49c2f60c9bb323451d3b5b92b7dd6
pawnesh <pawneshkitio@gmail.com>
2024-12-02 15:17:31 +0530

    dublicate header issue resolved

--- chat/index.php
+++ chat/index.php
@@ -1,4 +1,5 @@
 <?php 
+ob_start(); // Start output buffering to prevent "headers already sent" error
 include_once "header.php";
 include_once "../application/config/db.php";
 //$_SESSION['unique_id']='11';
@@ -12,4 +13,5 @@ else
 {
 	header("Location:login.php");
 	exit;
+	ob_end_flush(); // End output buffering and flush the content
 }?>
