b79bfee7ab3bd89ccf8c18a45309c51505479187
vikashxfriday <vikashxfriday@gmail.com>
2025-02-14 15:57:00 +0530

    Update Project Design and Task

--- application/views/admin/projects/card_view.php
+++ application/views/admin/projects/card_view.php
@@ -12,6 +12,7 @@ SELECT
     p.name AS project_name,
     p.description AS project_description,
     p.status AS project_status,
+	p.start_date AS project_start_date,
     p.deadline AS project_deadline,
     p.date_finished AS project_date_finished,
     p.progress AS project_progress,
@@ -57,7 +58,8 @@ foreach ($data as $row) {
             'project_name' => $row['project_name'],
             'project_description' => $row['project_description'],
             'project_status' => $row['project_status'],
-            'project_deadline' => $row['project_deadline'],
+            'project_start_date' => $row['project_start_date'],
+			'project_deadline' => $row['project_deadline'],
             'project_date_finished' => $row['project_date_finished'],
             'project_progress' => $row['project_progress'],
             'task_count' => $row['task_count'],
@@ -81,7 +83,8 @@ $projects = array_values($projects);
 // Output the result
 // print_r($projects);
 ?>
-<div class="row">
+
+<div class="row vikash">
     <?php 
     // Array of 4 colors
     $colorsProject = ['#FF5733', '#33FF57', '#3357FF', '#F0E130'];
@@ -96,36 +99,16 @@ $projects = array_values($projects);
             $finalShortName = $firstProjectLetter;
         }
     ?>
-    <div class="col-sm-4">
-        <div class="p-card">    
-        <!-- Header Section -->
-        <div class="p-header">
-            <div class="p-circle" style="background-color:<?=$color; ?>"><?= $finalShortName ?></div>
-            <div>
-                <a href="<?php echo admin_url('projects/view/' . $d['project_id']) ?>">
-                <h4 style="margin:0;"><?= $d['project_name'] ?> <span class="glyphicon glyphicon-eye-open" style="color: #3bc37b;"></span></h4>
-                </a>
-            </div>
-        </div>
-
-        <!-- Status -->
-        <div class="p-status-box">
-                <div class="p-status">
-                    <?php if($d['project_status']==2){ ?>
-                        Ongoing
-                    <?php }else{ ?>
-                        Completed
-                    <?php } ?>
-                </div>
-                <p><strong>Due Date:</strong><?= $d['project_deadline'] ?></p>
-            </div>
-        <div class="p-para">
-            <p class="text-muted " style="font-size: 14px;"><?= $d['project_description'] ?></p>
-        </div>
-        <!-- Members -->
-        <div>
-            <h5 class="text-muted"><strong>MEMBERS</strong></h5>
-            <div class="p-members">
+	<a href="<?php echo admin_url('projects/view/' . $d['project_id']) ?>">
+	
+    <div class="col-sm-4 ">
+        <div class="card">
+ <span class="text-muted toggle-menu-options main-item-options pull-right"><i class="fa-solid fa-calendar-days"></i> <?= $d['project_start_date'] ?> To <?=!empty($d['project_deadline']) ? $d['project_deadline'] : 'N/A'; ?></span>
+  <span class="title" title="<?= $d['project_name']; ?>"><?= substr($d['project_name'],0,30); ?></span>
+  <span class="desc"><?=!empty($d['project_description']) ? substr($d['project_description'],0,50) : '<p>N/A</p>'; ?>
+  </span>
+  <h5 class="text-muted"><strong>MEMBERS</strong></h5>
+  <div class="p-members">
                 <?php $members = $d['project_staff'];
                 // Array of 4 colors
                 $colors = ['#FF5733', '#33FF57', '#3357FF', '#F0E130'];
@@ -143,17 +126,33 @@ $projects = array_values($projects);
                     <?php 
                     }  } ?>
             </div>
-        </div>  
-        <!-- Stats -->
-        <div class="p-stats">
-            <div>
-            <span>Tasks</span>
-            </div>
-            <div>
-            <span><?= $d['task_count'] ?></span>
-            </div>
-        </div>
-        </div>
+			
+<?php if($d['project_status']==2){ $status="Ongoing"; $clr="#FF9900";}else{ $status="Completed"; $clr="#009933"; } ?>
+
+	<div class="buttons">
+  <span  class="button" style="background-color:<?=$clr;?> !important;">
+    <? if($status=="Completed"){ ?>
+    <i class="fa-regular fa-circle-check fa-2x tw-text-white"></i>
+	<?php }else{ ?>
+    <i class="fa-solid fa-cog fa-spin fa-2x tw-text-white"></i>
+	<?php } ?>
+
+<div class="button-text google" ><span><?=$status;?></span></div>
+                    
+   
+  </span>
+  <span  class="button">
+    <i class="tw-text-white task-circle"><?= $d['task_count']; ?></i>
+    <div class="button-text apple">
+      <span>Tasks</span>
     </div>
+  </span>
+</div>		
+
+
+</div>
+	 </div>
+   </a>
+
     <?php } ?>
 </div>
